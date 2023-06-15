51743da49b748de5d461e2ecd9ade82452f74a47
Dieryson Araujo <115048301+Dieryson@users.noreply.github.com>
2023-06-15 13:44:36 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using DeclarandoObj.Entities;
+using DeclarandoObj.Entities.Enums;
+namespace Objeto
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+
+            Console.WriteLine(txt);
+
+        }
+    }
+}
